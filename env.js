547dfa97007ee010e12a410572bf7b24e653b299
Leon Erath <leon-erath@hotmail.de>
2020-04-27 18:40:32 +0200

    fix: LogRocket fix

--- env.js
+++ env.js
@@ -2,6 +2,7 @@
 const vars = {
 	REACT_APP_BACKEND_URL: process.env.REACT_APP_BACKEND_URL,
 	REACT_APP_SENTRY_URL: process.env.REACT_APP_SENTRY_URL,
+	REACT_APP_LOG_ROCKET: process.env.REACT_APP_LOG_ROCKET,
 };
 
 const js = `window.env=${JSON.stringify(vars)}\n`;

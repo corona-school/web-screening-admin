2e03ac439a9f8dc67b62b27394c2acb4cc62373c
Leon Erath <leon-erath@hotmail.de>
2020-06-09 18:02:34 +0200

    feat: add bug reporting

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,25 @@ import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import "./index.less";
 import App from "./App";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+
+Bugsnag.start({
+	apiKey: "44993ed02460c98ff098ea82215a58e6",
+	plugins: [new BugsnagPluginReact()],
+});
+
+const ErrorBoundary = (Bugsnag.getPlugin("react") as any).createErrorBoundary(
+	React
+);
 
 const startApp = () => {
-	ReactDOM.render(<App />, document.getElementById("root"));
+	ReactDOM.render(
+		<ErrorBoundary>
+			<App />
+		</ErrorBoundary>,
+		document.getElementById("root")
+	);
 };
 
 startApp();

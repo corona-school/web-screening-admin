e40d0d90950a5425f0e69c1b61342095d22a8155
Jonas Wilms <jonaswilms2000@gmail.com>
2020-06-19 20:10:42 +0200

    Add Course Hook

---
+++ src/api/useCourses.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import { CourseState, ApiCourseUpdate, Course } from "../types/Course";
+import Axios from "axios";
+import { baseUrl } from "./urls";
+
+
+export default function useCourses() {
+    const [{ courses, loading }, setState] = useState({ courses: [] as Course[], loading: true });
+
+    async function loadCourses(query: { search?: string, courseState?: CourseState }) {
+        setState({ loading: true, courses: [] });
+        const { status, data: { courses } } = await Axios.post(`${baseUrl}courses`, query);
+
+        if(status !== 200)
+            throw new Error(`Failed to fetch courses with status ${status}`);
+
+        console.log("loaded courses", courses);
+        setState({ loading: false, courses });
+    }
+
+    /* Calls the API to update the course, then updates the course list */
+    async function updateCourse({ id }: Course, update: ApiCourseUpdate) {
+        const before = courses;
+
+        // Remove this to update silently:
+        setState({ loading: true, courses: [] });
+
+        const { status, data: { course } } = await Axios.post(`${baseUrl}course/${id}/update`, update);
+
+        if(status !== 200)
+            throw new Error(`Failed to update course ${id} with status ${status}`);
+
+        setState({
+            loading: false,
+            courses: before.map(it => it.id === course.id ? course : it)
+        });
+    }
+
+    // Initially load all courses
+    useEffect(() => { loadCourses({}) }, []);
+
+    return { courses, loading, loadCourses, updateCourse };
+}

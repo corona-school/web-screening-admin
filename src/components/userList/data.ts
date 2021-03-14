22b2da4784ac0e5ca931fb544eb43c4e3b5c9230
Lennard Schiefelbein <lennard@familie-schiefelbein.de>
2021-03-14 16:22:24 +0100

    Add "Sachkunde" and "Deutsch als Zweitsprache" as additional subjects
    Fixes corona-school/project-user#212 and corona-school/project-user#282

--- src/components/userList/data.ts
+++ src/components/userList/data.ts
@@ -5,6 +5,7 @@ export const SchoolSubjects = [
   'Informatik',
   'Physik',
   'Chemie',
+  'Sachkunde',
   'Politik',
   'Wirtschaft',
   'Geschichte',
@@ -20,6 +21,7 @@ export const SchoolSubjects = [
   'Spanisch',
   'Italienisch',
   'Chinesisch',
+  'Deutsch als Zweitsprache',
 ];
 
 export const SchoolClasses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
